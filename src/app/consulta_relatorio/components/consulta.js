ea3d74053cf16ef588616a28942c1ab0af28ced3
Iann Molendolff <iannmolendolff@gmail.com>
2025-04-24 18:07:53 -0300

    componente consulta

--- src/app/consulta_relatorio/components/consulta.js
+++ src/app/consulta_relatorio/components/consulta.js
@@ -2,7 +2,21 @@ import React from 'react'
 
 export default function Consulta(){
   return (
-    <div>consulta</div>
+    <div className='border rounded-2xl bg-slate-50 '>
+      <header className='bg-amber-400 rounded-2xl p-5 '>
+      consulta
+
+      </header>
+      <div className='flex flex-column text-black p-2 items-start justify-items-start'>
+        <a className='bg-gray-200 w-full'>Requerimentos</a>
+        <a className='bg-gray-200 w-full'>Listagem</a>
+        <a className='bg-gray-200 w-full'>Boletim</a>
+        <a>Diario</a>
+        <a>Matriz</a>
+        <a>Pedidos de matricula</a>
+        <a>Alteração de nota</a>
+      </div>
+      </div>
   )
 }
 
--- src/app/consulta_relatorio/page.js
+++ src/app/consulta_relatorio/page.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import Header from '../matricula/components/header/header'
 import Consulta from './components/consulta'
+import Relatorio from './components/relatorio'
 export default function Home(){
   return (
     <div > 
-      page
+      <Header/>
+      <div>
+        <div className='flex justify-center items-center'>
+          <h1>Registro Escolar</h1>
+        </div>
+          <div className=''>
+        <div className='flex justify-center items-center m-10'>
+            <div className='m-3'>
+              <Consulta/>
+            </div>
+            <div>
+              <Relatorio/>
+            </div>
+          </div>
+        </div>
+
+        
+      </div>
 
 
 
