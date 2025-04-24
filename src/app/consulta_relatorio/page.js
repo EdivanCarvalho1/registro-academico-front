55a0561f089f3a4802830dcd927559d6555296c5
Iann Molendolff <iannmolendolff@gmail.com>
2025-04-24 16:26:05 -0300

    issue5

---
+++ src/app/consulta_relatorio/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../matricula/components/header/header'
+import Consulta from './components/consulta'
+export default function Home(){
+  return (
+    <div > 
+      page
+
+
+
+    </div>
+  )
+}
