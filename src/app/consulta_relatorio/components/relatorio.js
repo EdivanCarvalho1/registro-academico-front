c75ac67aadc56f998b108dbc3af96460330f9d30
Iann Molendolff <iannmolendolff@gmail.com>
2025-04-24 18:10:55 -0300

    componente relatorio

--- src/app/consulta_relatorio/components/relatorio.js
+++ src/app/consulta_relatorio/components/relatorio.js
@@ -2,7 +2,21 @@ import React from 'react'
 
 export default function Relatorio(){
   return (
-    <div>relatorio</div>
+    <div className='border rounded-2xl bg-slate-50 '>
+    <header className='bg-amber-400 rounded-2xl p-5 '>
+    consulta
+
+    </header>
+    <div className='flex flex-column text-black p-2 items-start justify-items-start'>
+      <a className='bg-gray-200 w-full'>Requerimentos</a>
+      <a className='bg-gray-200 w-full'>Listagem</a>
+      <a className='bg-gray-200 w-full'>Boletim</a>
+      <a className='bg-gray-200 w-full'>Diario</a>
+      <a className='bg-gray-200 w-full'>Matriz</a>
+      <a className='bg-gray-200 w-full'>Pedidos de matricula</a>
+      <a className='bg-gray-200 w-full'>Alteração de nota</a>
+    </div>
+    </div>
   )
 }
 
