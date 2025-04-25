f206923cc5b6696b511622ad2e0aaaac66b0199b
Iann Molendolff <iannmolendolff@gmail.com>
2025-04-24 23:50:24 -0300

    correcao de cores

--- src/app/consulta_relatorio/components/consulta.js
+++ src/app/consulta_relatorio/components/consulta.js
@@ -9,12 +9,12 @@ export default function Consulta({ color }){
       </header>
       <div className='flex flex-column text-black justify-items-start'>
         <a href='#' className='bg-gray-200 w-full rounded-sm text-black text-decoration-none p-2  '>Requerimentos</a>
-        <a href='#' className='bg-gray-200 w-full rounded-sm text-black text-decoration-none p-2'>Listagem</a>
+        <a href='#' className='bg-gray-200 w-full rounded-sm text-black text-decoration-none p-2 '>Listagem</a>
         <a href='#' className='bg-gray-200 w-full rounded-sm text-black text-decoration-none p-2'>Boletim</a>
-        <a href='#' className='text-decoration-none text-black p-2'>Diario</a>
-        <a href='#' className='text-decoration-none text-black p-2'>Matriz</a>
-        <a href='#' className='text-decoration-none text-black p-2'>Pedidos de matricula</a>
-        <a href='#' className='text-decoration-none text-black p-2'>Alteração de nota</a>
+        <a href='#' className='bg-gray-200 w-full rounded-sm text-black text-decoration-none p-2 '>Diario</a>
+        <a href='#' className='bg-gray-200 w-full rounded-sm text-black text-decoration-none p-2 '>Matriz</a>
+        <a href='#' className='bg-gray-200 w-full rounded-sm text-black text-decoration-none p-2' >Pedidos de matricula</a>
+        <a href='#' className='bg-gray-200 w-full rounded-sm text-black text-decoration-none p-2 '>Alteração de nota</a>
       </div>
       </div>
   )
--- src/app/consulta_relatorio/page.js
+++ src/app/consulta_relatorio/page.js
@@ -6,27 +6,29 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-purple-700 flex justify-center">
+    <div className="min-h-screen bg-[#493159] flex justify-center">
       <div className="bg-white w-full max-w-screen-xl">
         <Header />
         <div className="w-full">
           <div className="flex justify-center items-center mt-10">
-              <Image src="/icons8-voltar-32.png"
-              width={30}
-              height={30}
-              alt="voltar"
-              />
-            <h1 className="text-2xl text-black font-bold">
+            <h1 className="text-2xl text-black font-bold inline-flex">
+              <div>
+                <Image src="/icons8-voltar-32.png"
+                width={50}
+                height={40}
+                alt="voltar"
+                />
+              </div>
               Registro Escolar</h1>
           </div>
           <div className="flex flex-col m-7 justify-center items-center">
             <div className="inline-flex">
-              <Consulta color="bg-amber-300" />
-              <Relatorio color="bg-amber-300" />
+              <Consulta color="bg-[#f9cf64]" />
+              <Relatorio color="bg-amber-300 " />
             </div>
             <div className="mt-4 inline-flex">
               <Consulta color="bg-purple-400" />
-              <Relatorio color="bg-purple-400" />
+              <Relatorio color="bg-[#b697e8]" />
             </div>
           </div>
         </div>
