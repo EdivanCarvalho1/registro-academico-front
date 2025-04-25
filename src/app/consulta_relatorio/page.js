8d08d82d654837a403a5e449b65803e6fc6c7e85
Iann Molendolff <iannmolendolff@gmail.com>
2025-04-24 23:03:44 -0300

    correcao tamanho da pagina

--- src/app/consulta_relatorio/page.js
+++ src/app/consulta_relatorio/page.js
@@ -5,25 +5,21 @@ import Relatorio from './components/relatorio'
 
 export default function Home() {
   return (
-    <div className="bg-purple-700 min-h-screen flex justify-center">
+    <div className="min-h-screen bg-purple-700 flex justify-center">
       <div className="bg-white w-full max-w-screen-xl">
         <Header />
         <div className="w-full">
-          <div className="h-screen">
-            <div className="flex justify-center items-center">
-              <h1 className="text-2xl font-bold">Registro Escolar</h1>
+          <div className="flex justify-center items-center mt-10">
+            <h1 className="text-2xl text-black font-bold">Registro Escolar</h1>
+          </div>
+          <div className="flex flex-col m-7 justify-center items-center">
+            <div className="inline-flex">
+              <Consulta color="bg-amber-300" />
+              <Relatorio color="bg-amber-300" />
             </div>
-            <div className="flex flex-col m-7 justify-center items-center">
-              <div className="inline-flex">
-                <Consulta color="bg-amber-300" />
-                <Relatorio color="bg-amber-300" />
-              </div>
-              <div className="mt-4">
-                <div className="inline-flex">
-                  <Consulta color="bg-purple-400" />
-                  <Relatorio color="bg-purple-400" />
-                </div>
-              </div>
+            <div className="mt-4 inline-flex">
+              <Consulta color="bg-purple-400" />
+              <Relatorio color="bg-purple-400" />
             </div>
           </div>
         </div>
