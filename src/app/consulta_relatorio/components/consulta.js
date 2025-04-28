23bd6258d150d98148da08ca2ed658ebb47142a5
Iann Molendolff <iannmolendolff@gmail.com>
2025-04-27 21:43:03 -0300

    botão matricula aluno e alinhamento de de itens

--- src/app/consulta_relatorio/components/consulta.js
+++ src/app/consulta_relatorio/components/consulta.js
@@ -3,20 +3,19 @@ import React from 'react'
 
 export default function Consulta({ color }){
   return (
-    <div className=' rounded-lg bg-slate-50 m-5 w-72 h-full '>
+    <div className=' rounded-lg bg-slate-50 ml-2 mr-12 w-72 h-full '>
       <header className={`rounded-sm ${color} p-2 `}>
         Consultas
       </header>
-      <div className='flex flex-column text-black justify-items-start'>
-        <a href='#' className='bg-gray-200 w-full rounded-sm text-black text-decoration-none p-2  '>Requerimentos</a>
-        <a href='#' className='bg-gray-200 w-full rounded-sm text-black text-decoration-none p-2 '>Listagem</a>
-        <a href='#' className='bg-gray-200 w-full rounded-sm text-black text-decoration-none p-2'>Boletim</a>
-        <a href='#' className='bg-gray-200 w-full rounded-sm text-black text-decoration-none p-2 '>Diario</a>
-        <a href='#' className='bg-gray-200 w-full rounded-sm text-black text-decoration-none p-2 '>Matriz</a>
-        <a href='#' className='bg-gray-200 w-full rounded-sm text-black text-decoration-none p-2' >Pedidos de matricula</a>
-        <a href='#' className='bg-gray-200 w-full rounded-sm text-black text-decoration-none p-2 '>Alteração de nota</a>
+      <div className='flex flex-column text-black justify-items-start '>
+        <a href='#' className='bg-gray-200 w-full rounded-sm text-black text-decoration-none p-2 hover:font-bold '>Requerimentos</a>
+        <a href='#' className='bg-gray-200 w-full rounded-sm text-black text-decoration-none p-2 hover:font-bold'>Listagem</a>
+        <a href='#' className='bg-gray-200 w-full rounded-sm text-black text-decoration-none p-2 hover:font-bold'>Boletim</a>
+        <a href='#' className='bg-gray-200 w-full rounded-sm text-black text-decoration-none p-2 hover:font-bold'>Diario</a>
+        <a href='#' className='bg-gray-200 w-full rounded-sm text-black text-decoration-none p-2 hover:font-bold'>Matriz</a>
+        <a href='#' className='bg-gray-200 w-full rounded-sm text-black text-decoration-none p-2 hover:font-bold' >Pedidos de matricula</a>
+        <a href='#' className='bg-gray-200 w-full rounded-sm text-black text-decoration-none p-2 hover:font-bold'>Alteração de nota</a>
       </div>
       </div>
-  )
+      )
 }
-
